2c51b2aa1fc7b19fede671d4b876edf6fe101deb
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-03-17 12:25:10 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const drivers = ["Mell", "Gabby", "Champ", "Cooper"];
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2) // returns Mell and Gabby
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2) // returns Champ and Cooper
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = (integer) => function (fare) {
+  return fare * integer; //multiplies the fare by an interger
+}
+const fareDoubler = (amount) => amount * 2 //doubles the fare
+const fareTripler = (amount) => amount * 3 // tripples the fare
+const selectDifferentDrivers = (drivers, driverSelector) => driverSelector(drivers) // returns either returnFirstTwoDrivers or returnLastTwoDrivers
